7473781960ed9f995b141684e61603e91cacc793
zhouxinchen <xinchenzhou666@gmail.com>
2022-03-01 12:30:27 +0800

    ''

--- src/main/java/cn/omisheep/authz/core/util/ValueMatcher.java
+++ src/main/java/cn/omisheep/authz/core/util/ValueMatcher.java
@@ -54,33 +54,34 @@ public class ValueMatcher {
 
     private static Object parse(String value, Class<?> type) {
         if (type.equals(String.class)) return value;
-        if (type.equals(Integer.class) || type.equals(Integer.TYPE)) {
+        if (type.equals(Integer.class) || type.equals(int.class)) {
             return new Integer(value);
-        } else if (type.equals(Long.class) || type.equals(Long.TYPE)) {
+        } else if (type.equals(Long.class) || type.equals(long.class)) {
             return new Long(value);
-        } else if (type.equals(Short.class) || type.equals(Short.TYPE)) {
+        } else if (type.equals(Short.class) || type.equals(short.class)) {
             return new Short(value);
-        } else if (type.equals(Double.class) || type.equals(Double.TYPE)) {
+        } else if (type.equals(Double.class) || type.equals(double.class)) {
             return new Double(value);
-        } else if (type.equals(Float.class) || type.equals(Float.TYPE)) {
+        } else if (type.equals(Float.class) || type.equals(float.class)) {
             return new Float(value);
-        } else if (type.equals(Character.class) || type.equals(Character.TYPE)) {
+        } else if (type.equals(Character.class) || type.equals(char.class)) {
             return value.charAt(0);
-        } else if (type.equals(Boolean.class) || type.equals(Boolean.TYPE)) {
+        } else if (type.equals(Boolean.class) || type.equals(boolean.class)) {
             return Boolean.valueOf(value);
-        } return null;
+        }
+        return null;
     }
 
     public static ValueType checkType(Class<?> type) {
         if (type.equals(String.class) || type.equals(Boolean.class) || type.equals(boolean.class))
             return ValueType.EQUALS;
 
-        if (type.equals(Integer.class) || type.equals(Integer.TYPE)
-                || type.equals(Long.class) || type.equals(Long.TYPE)
-                || type.equals(Short.class) || type.equals(Short.TYPE)
-                || type.equals(Double.class) || type.equals(Double.TYPE)
-                || type.equals(Float.class) || type.equals(Float.TYPE)
-                || type.equals(Character.class) || type.equals(Character.TYPE))
+        if (type.equals(Integer.class) || type.equals(int.class)
+                || type.equals(Long.class) || type.equals(long.class)
+                || type.equals(Short.class) || type.equals(short.class)
+                || type.equals(Double.class) || type.equals(double.class)
+                || type.equals(Float.class) || type.equals(float.class)
+                || type.equals(Character.class) || type.equals(char.class))
             return ValueType.RANGE;
 
         return ValueType.OTHER;
