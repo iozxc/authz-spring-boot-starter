233c591d850718b1deb7d956580858561754bcda
zhouxinchen <xinchenzhou666@gmail.com>
2022-08-31 01:43:06 +0800

    ''

--- src/main/java/cn/omisheep/authz/core/util/IPUtils.java
+++ src/main/java/cn/omisheep/authz/core/util/IPUtils.java
@@ -12,9 +12,9 @@ public class IPUtils {
 
     public static String getIp(HttpServletRequest request) {
         String ip = null;
-        for (int i = 0; i < IP_HEADERS.length; i++) {
-            ip = request.getHeader(IP_HEADERS[i]);
-            if (check(ip)) break;
+        for (String ipHeader : IP_HEADERS) {
+            ip = request.getHeader(ipHeader);
+            if (!check(ip)) break;
         }
         if (check(ip)) ip = request.getRemoteAddr();
         if (ip.length() > 15 && !ip.contains(":")) {
@@ -26,7 +26,7 @@ public class IPUtils {
                 }
             }
         }
-        return ip.equals(LOCAL_V6) ? LOCAL : ip;
+        return LOCAL_V6.equals(ip) ? LOCAL : ip;
     }
 
     private static boolean check(String ip) {
@@ -36,6 +36,6 @@ public class IPUtils {
     private static final String   UNKNOWN    = "unknown";
     private static final String   LOCAL      = "127.0.0.1";
     private static final String   LOCAL_V6   = "0:0:0:0:0:0:0:1";
-    private static final String[] IP_HEADERS = {"x-forwarded-for", "X-FORWARDED-FOR", "Proxy-Client-IP", "WL-Proxy-Client-IP", "HTTP_CLIENT_IP", "HTTP_X_FORWARDED_FOR", "X-Real-IP"};
+    private static final String[] IP_HEADERS = {"x-forwarded-for", "Proxy-Client-IP", "WL-Proxy-Client-IP", "HTTP_CLIENT_IP", "HTTP_X_FORWARDED_FOR", "X-Real-IP"};
 
 }
